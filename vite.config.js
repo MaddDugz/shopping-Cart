de9c2bf5b95c019913130d2ceca40bc384b8f0db
Anthony27 <ezeugwuanthony456@gmail.com>
2025-12-04 17:27:28 +0100

    Edited the vite.config

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base : process.env.VITE ? '/' : '/shopping-Cart/'
+  base : process.env.VERCEL ? '/' : '/shopping-Cart/'
 })
