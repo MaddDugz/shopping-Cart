24c8b5cb9c63c629de6218c55afbc37cef5e36cb
Anthony27 <ezeugwuanthony456@gmail.com>
2025-12-04 18:43:15 +0100

    Test

--- src/Shopping/home.jsx
+++ src/Shopping/home.jsx
@@ -85,6 +85,8 @@ function Home() {
                         ? data
                         : data.products || [])  // Handle different response structures
 
+            console.log(data.length)
+            console.log(products.length)
             if(products.length === 0){
               setError("No Products Available")
           }
